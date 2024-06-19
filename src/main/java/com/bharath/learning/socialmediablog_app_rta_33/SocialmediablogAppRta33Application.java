1182c816bd48c06d3e3900943f64532ba4da01e4
Bharath Ashok <iambharath.ashoka@gmail.com>
2024-06-19 06:34:00 +0530

    Initial Commit For Social Media Blog App RTA 33

---
+++ src/main/java/com/bharath/learning/socialmediablog_app_rta_33/SocialmediablogAppRta33Application.java
@@ -0,0 +1,13 @@
+package com.bharath.learning.socialmediablog_app_rta_33;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmediablogAppRta33Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmediablogAppRta33Application.class, args);
+	}
+
+}
