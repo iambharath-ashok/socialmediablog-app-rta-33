d2f1169d4e1789d761a42b9004d541b889f8126e
Bharath Ashok <iambharath.ashoka@gmail.com>
2024-06-19 06:46:58 +0530

    Intial Project Setup files of Social Media Blog App 33 for main branch

---
+++ src/test/java/com/bharath/learning/socialmediablog_app_rta_33/SocialmediablogAppRta33ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharath.learning.socialmediablog_app_rta_33;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmediablogAppRta33ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
